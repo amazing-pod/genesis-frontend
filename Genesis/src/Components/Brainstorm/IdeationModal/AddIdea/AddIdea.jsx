546cd44908c8c483088253bc0efbbed920f5289a
Brenda Nallely Aceves Limon <baceveslimon@baceves-ltmvyr6.internal.salesforce.com>
2024-07-23 08:54:36 -0700

    Completed styling for IdeationModal sub-modals: generating project ideas, generating project features and adding project ideas

--- Genesis/src/Components/Brainstorm/IdeationModal/AddIdea/AddIdea.jsx
+++ Genesis/src/Components/Brainstorm/IdeationModal/AddIdea/AddIdea.jsx
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import "./AddIdea.css";
 import add_idea_graphic from "../../../../assets/png/add_idea.png"
-// Import main modal
+// Import relevant modals
 import IdeationModal from '../IdeationModal';
+import Dropdown from "../../../Shared/Dropdown/Dropdown";
 
 
 const AddIdea = ( {closeModal}) => {
@@ -11,7 +12,6 @@ const AddIdea = ( {closeModal}) => {
 
 
     const addProjectIdea = () => {
-        // Update database here, before modal closes.
         closeModal();
     }
 
@@ -20,7 +20,7 @@ const AddIdea = ( {closeModal}) => {
         if (newFeatureText.trim() !== '') {
         const updatedFeatures = [...editedFeatures, newFeatureText];
         setEditedFeatures(updatedFeatures);
-          setNewFeatureText(''); // Clear the input after adding
+            setNewFeatureText('');
         }
     };
 
@@ -30,56 +30,46 @@ const AddIdea = ( {closeModal}) => {
 
     return (
         <>
-        <div className="modal">
-            <div className="add-idea-content">
-                <div className="add-idea-info-container">
+        <>
+        <div className="ideation-add-project">
+            <div className="ideation-feature-user-info">
                 <h2>Add a new project idea</h2>
                 <p>Enter the information below to add a new project to your list</p>
-            <input type="text" placeholder='Project Name' />
-            <p>Enter your intended project features below</p>
-            <h2>Project Category:</h2>
-            <select name="option" id="add-idea-dropdown" placeholder="Choose a category">
-                <option value="Environment">Environment</option>
-                <option value="Environment">Technology</option>
-                <option value="Environment">Healthcare</option>
-                <option value="Environment">News</option>
-                <option value="Environment">Gaming</option>
-                <option value="Environment">Education</option>
-            </select>
-            {/* Add respective items heree */}
-            <h2>Features list</h2>
-            <div className="add-item-container">
-                <input
-                type="text"
-                value={newFeatureText}
-                onChange={handleNewFeatureChange}
-                placeholder="Enter new feature"
-                className="new-feature-add"
-                />
-                <div className="add-feature-buttons">
-                    <button onClick={handleFeatureAdd}>Save</button>
-                    <button onClick={() => setNewFeatureText('')}>Clear</button>
-                </div>
-                </div>
-                <>
-                
+                {/* Add respective items here */}
+                <h2>Project Name:</h2>
+                <input type="text" placeholder='Project Title...'/>
+                <h2>Features:</h2>
                 {editedFeatures.map((feature, index) => (
-                <p key={index}>{index + 1}. {feature}</p>
+                    <p key={index}>{index + 1}. {feature}</p>
                 ))}
-            </>
-            <div className="add-feature-buttons">
-            <button onClick={addProjectIdea} >Finish</button>
-            <button onClick={closeModal}>Go Back</button>
-            </div>
+                <div className="add-item-container">
+                    {/* <textarea name="" id=""></textarea> */}
+                    <textarea
+                    type="text"
+                    value={newFeatureText}
+                    onChange={handleNewFeatureChange}
+                    placeholder="Enter new issue"
+                    className="user-idea-info"
+                    >
+                    </textarea>
+                    <div className="add-project-buttons">
+                        <button onClick={handleFeatureAdd}>Save</button>
+                        <button onClick={() => setNewFeatureText('')}>Clear</button>
+                    </div>
 
-            </div>
-            
-            <img src={add_idea_graphic} alt="Add Idea Graphic" />
-        </div>
-        </div>
+                    </div>
+                    {/* Take user to main modal */}
+                    <div className="project-ideation-button-container">
+                        <button className="generate" onClick={closeModal}>Finish</button>
+                        <button className="backtrack" onClick={closeModal}>Go Back</button>
+                    </div>
+                </div>
 
-
-
+                <>
+                </>
+                <img className="add-ideas-image" src={add_idea_graphic} alt="Project Ideation Image" />                
+                </div>
+                </>                
 
         </>
     );
--- Genesis/src/Components/Brainstorm/IdeationModal/IdeationFeature/IdeationFeature.jsx
+++ Genesis/src/Components/Brainstorm/IdeationModal/IdeationFeature/IdeationFeature.jsx
@@ -1,11 +1,65 @@
 import React, {useState, useEffect} from 'react';
 import './IdeationFeature.css';
-// Import main modal
-import IdeationModal from '../IdeationModal';
+import generate_features_image from  "../../../../assets/png/generate_features.png"
+import Dropdown from '../../../Shared/Dropdown/Dropdown';
+import add_inactive_icon from "../../../../assets/png/add_inactive_purple.png";
+import add_active_icon from "../../../../assets/png/add_active.png"
 
 
 const IdeationFeature = ( {closeModal} ) => {
     const [step, setStep] = useState(0);
+    const [newFeatureText, setNewFeatureText] = useState('');
+    const [editedFeatures, setEditedFeatures] = useState([]);
+    // Populate with actual idea array
+    const ideaList = ['a', 'b', 'c'];
+
+
+    const ideationFeatureDummyData = [
+        {
+            id: 1,
+            name: "Voice Control",
+            description: "Allow users to control devices using voice commands.",
+        },
+        {
+            id: 2,
+            name: "Energy Monitoring",
+            description: "Track energy consumption of connected devices.",
+        },
+        {
+            id: 3,
+            name: "Remote Access",
+            description: "Enable users to control home devices from anywhere via app.",
+        },
+        {
+            id: 4,
+            name: "Security Cameras",
+            description: "Integrate security cameras for monitoring purposes.",
+        },
+        {
+        id: 5,
+        name: "Automated Irrigation",
+        description: "Automate watering schedules for garden and plants.",
+        },
+    ];      
+
+
+    const addProjectIdea = () => {
+        // Update database here, before modal closes.
+        closeModal();
+    }
+
+
+    const handleFeatureAdd = () => {
+        if (newFeatureText.trim() !== '') {
+        const updatedFeatures = [...editedFeatures, newFeatureText];
+        setEditedFeatures(updatedFeatures);
+          setNewFeatureText(''); // Clear the input after adding
+        }
+    };
+
+    const handleNewFeatureChange = (e) => {
+        setNewFeatureText(e.target.value);
+    };
 
     const addStep = () => {
         setStep(step + 1);
@@ -15,45 +69,124 @@ const IdeationFeature = ( {closeModal} ) => {
         setStep(step - 1);
     }
 
+    
+
     const renderModalContent = () => {
         switch (step) {
             // When step is 0, go back to main modal
             case 0:
                 return (
                     <>
-                        <p>Step 0: Generate ideation project works!</p>
-                        {/* Take user to main modal */}
-                        <button onClick={addStep}>Proceed</button>
-                        <button onClick={closeModal}>Go Back</button>
+                    <div className="ideation-feature-user-info">
+                    <h2>Add New Features</h2>
+                    <hr />
+                    <p>To generate project features, complete either of the 
+                        either of the following options:
+                    </p>
+                    <p>1. Choose an existing idea:</p>
+                    <Dropdown dropdownPlaceholder={"Choose an idea:"} dropdownOptions={ideaList}/>
+
+                    <p>2. Otherwise, enter your project description </p>
+                    <textarea name="items" id=""></textarea>
+                    
+                    <div className="ideation-feature-button-container">
+                        <button className="generate" onClick={addStep}>Generate</button>
+                        <button className="backtrack" onClick={closeModal}>Go Back</button>
+                    </div>
+                    </div>
+
+                    <img className="generate-features-image" src={generate_features_image} alt="generate features image" />
                     </>
                 );
-            // Base Case: User is on main project ideas section
+
+
             case 1:
+                // Should only show to a user if they chose to enter a project description 
                 return (
                     <>
-                        <p>Step 1: Generate ideation features works!</p>
-                        {/* Take user to main modal */}
-                        <button onClick={addStep}>Proceed</button>
-                        <button onClick={backtrackStep}>Go Back</button>
+                    <div className="ideation-project-user-info">
+                    <h2>Create Project Features</h2>
+                        <p>Based on your previous project description, tell us any features you intend on implementing:</p>
+
+                {/* Add respective items here */}
+                <h2>Issues:</h2>
+                {editedFeatures.map((feature, index) => (
+                    <p key={index}>{index + 1}. {feature}</p>
+                ))}
+                <div className="add-item-container">
+                    {/* <textarea name="" id=""></textarea> */}
+                    <textarea
+                    type="text"
+                    value={newFeatureText}
+                    onChange={handleNewFeatureChange}
+                    placeholder="Enter new issue"
+                    className="user-idea-info"
+                    >
+                    </textarea>
+
+                    <div className="add-project-buttons">
+                        <button onClick={handleFeatureAdd}>Save</button>
+                        <button onClick={() => setNewFeatureText('')}>Clear</button>
+                    </div>
+                    </div>
+                    <>
                     </>
+                        {/* Take user to main modal */}
+                        <div className="project-ideation-button-container">
+                            <button className="generate" onClick={addStep}>Generate</button>
+                            <button className="backtrack" onClick={closeModal}>Go Back</button>
+                        </div>
+                        
+                    </div>
+                    <img className="generate-projects-image" src={generate_features_image} alt="Project Ideation Image" />
+                    </>                
                 );
+            // Base Case: User is on main project ideas section
             case 2:
                 return (
                     <>
-                        <p>Step 2, List of generated user features: </p>
-                        {/* Take user to main modal */}
-                        <button onClick={closeModal}>Go Back</button>
-                        <button onClick={backtrackStep}>Go Back</button>
+                    <div className="ideation-project-results" >
+                        <h2>Results</h2>
+                        <p>Based on your responses, we believe the following project ideas 
+                            may be best suited for you. To add an idea to your list, click
+                            the plus icon to the right of the project. <b>Scroll</b> down to see all generated ideas</p>
+                        
+                        {/* Generated Feature Results: */}
+                        <div className="ideation-project-all-results">
+                        {ideationFeatureDummyData.map((feature, index) => (
+                        <>
+                        <div className="ideation-feature-result">
+                            <div className="ideation-feature-info">
+                            <h2>{index+1}. {feature.name}</h2>
+                            <p>{feature.description}</p>
+                            </div>
+                            <img src={add_inactive_icon} alt="add-inactive-icon" />      
+                        </div>
+                        </>
+                        ))}
+                        </div>
+
+                        
+                        
+                        {/* Modal Actions + Navigations */}
+                        <div className="project-ideation-button-container">
+                            <button className="generate" onClick={closeModal}>Finish</button>
+                            <button className="generate">Regenerate</button>
+                            <button className="backtrack" onClick={backtrackStep}>Go Back</button>
+                        </div>
+
+
+
+                    </div>
                     </>
                 );
         }
     };
 
+
     return (
-        <div className="modal">
-            <div className="ideation-modal-content">
-                {renderModalContent()}
-            </div>
+        <div className="ideation-feature-modal-container">
+            {renderModalContent()}
         </div>
     );
 };
