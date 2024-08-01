c783a534ab772be60491df269c6e93f516eba3c1
Brenda Nallely Aceves Limon <baceveslimon@baceves-ltmvyr6.internal.salesforce.com>
2024-08-01 12:48:21 -0700

    User making a reply to a comment works

--- Genesis/src/components/Community/ForumPost/ReplyForm.jsx
+++ Genesis/src/components/Community/ForumPost/ReplyForm.jsx
@@ -2,39 +2,34 @@ import React, { useState } from "react";
 import "./ReplyForm.css";
 
 const ReplyForm = ({ commentId, onReplySubmit, onCancel }) => {
-	const [newReply, setNewReply] = useState("");
+    const [newReply, setNewReply] = useState("");
 
-	const handleReplyChange = (e) => {
-		setNewReply(e.target.value);
-	};
+    const handleReplyChange = (e) => {
+        setNewReply(e.target.value);
+    };
 
-	const handleReplySubmit = (e) => {
-		e.preventDefault();
-		onReplySubmit(commentId, newReply);
-		setNewReply("");
-	};
+    const handleReplySubmit = (e) => {
+        e.preventDefault();
+        onReplySubmit(commentId, newReply);
+        setNewReply("");
+    };
 
-	return (
-		<form
-			className="reply-form"
-			onSubmit={() => {
-				handleReplySubmit(commentId, newReply);
-			}}
-		>
-			<textarea
-				value={newReply}
-				onChange={handleReplyChange}
-				placeholder="Write a reply..."
-				required
-			/>
-			<div className="reply-form-buttons">
-				<button type="submit">Post Reply</button>
-				<button type="button" onClick={onCancel}>
-					Cancel
-				</button>
-			</div>
-		</form>
-	);
+    return (
+        <form className="reply-form" onSubmit={handleReplySubmit}>
+            <textarea
+                value={newReply}
+                onChange={handleReplyChange}
+                placeholder="Write a reply..."
+                required
+            />
+            <div className="reply-form-buttons">
+                <button type="submit">Post Reply</button>
+                <button type="button" onClick={onCancel}>
+                    Cancel
+                </button>
+            </div>
+        </form>
+    );
 };
 
 export default ReplyForm;
