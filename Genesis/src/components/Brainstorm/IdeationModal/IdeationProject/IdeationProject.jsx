97657250f61ce6f5289ab667da8ea8aa9afe435d
Brenda Nallely Aceves Limon <baceveslimon@baceves-ltmvyr6.internal.salesforce.com>
2024-08-02 13:57:14 -0700

    Added animation between generation and results for ideation

--- Genesis/src/components/Brainstorm/IdeationModal/IdeationProject/IdeationProject.jsx
+++ Genesis/src/components/Brainstorm/IdeationModal/IdeationProject/IdeationProject.jsx
@@ -13,6 +13,7 @@ import { useDropdown } from "../../../../context/DropdownContext";
 import { useProject } from "../../../../context/ProjectContext";
 
 const IdeationProject = ({ closeModal }) => {
+	const [isLoading, setIsLoading] = useState(true);
 	const [step, setStep] = useState(0);
 	const [newFeatureText, setNewFeatureText] = useState("");
 	const [editedIssues, setEditedIssues] = useState([]);
@@ -81,62 +82,77 @@ const IdeationProject = ({ closeModal }) => {
 	};
 
 	const addStep = () => {
+		setIsLoading(true); // Set loading state to true
 		const generateIdeas = async () => {
-			const response = await axios.post(
-				`${import.meta.env.VITE_GENESIS_API_URL}/api/chat`,
-				{
-					prompt: `Generate a list of 5 distinct project ideas given the category: ${option} and the issues: ${editedIssues}. Each idea should include a highly appropriate title, a useful description, a highly accurate impact rating, a highly accurate feasibility rating, and a highly accurate difficulty rating. The list should always be in the following format exactly: [{title: 'Virtual Classroom Enhancer', description: 'A tool to create interactive and engaging virtual classrooms with real-time collaboration features.', impact: 5, feasibility: 4, difficulty: 3},{title: 'AI-Powered Tutoring Assistant', description: 'An AI-driven tutoring assistant that provides personalized help to students based on their learning progress.', impact: 4, feasibility: 3, difficulty: 4},{title: 'Gamified Learning Platform', description: 'A platform that uses game mechanics to make remote learning more engaging and motivating for students.', impact: 4, feasibility: 3, difficulty: 3},{title: 'Remote Lab Simulator', description: 'A simulator that allows students to conduct virtual lab experiments and gain hands-on experience remotely.', impact: 5, feasibility: 3, difficulty: 4},{title: 'Collaborative Study Space', description: 'An online space where students can study together, share resources, and support each other's learning.', impact: 3, feasibility: 5, difficulty: 2}]. Please generate the ideas. Never use new line. Never end with period`,
-				}
-			);
-			console.log(response.data);
-
-			let generatedIdeas = JSON.parse(
-				response.data.response
-					.replace(/([{,]\s*)(\w+):/g, '$1"$2":')
-					.replace(/'/g, '"')
-			);
-
-			generatedIdeas = generatedIdeas.map((object) => ({
-				...object,
-				category: option,
-			}));
-
-			console.log(generatedIdeas);
-
-			setIdeationProjectData(generatedIdeas);
-			setStep(step + 1);
+			try {
+				const response = await axios.post(
+					`${import.meta.env.VITE_GENESIS_API_URL}/api/chat`,
+					{
+						prompt: `Generate a list of 5 distinct project ideas given the category: ${option} and the issues: ${editedIssues}. Each idea should include a highly appropriate title, a useful description, a highly accurate impact rating, a highly accurate feasibility rating, and a highly accurate difficulty rating. The list should always be in the following format exactly: [{title: 'Virtual Classroom Enhancer', description: 'A tool to create interactive and engaging virtual classrooms with real-time collaboration features.', impact: 5, feasibility: 4, difficulty: 3},{title: 'AI-Powered Tutoring Assistant', description: 'An AI-driven tutoring assistant that provides personalized help to students based on their learning progress.', impact: 4, feasibility: 3, difficulty: 4},{title: 'Gamified Learning Platform', description: 'A platform that uses game mechanics to make remote learning more engaging and motivating for students.', impact: 4, feasibility: 3, difficulty: 3},{title: 'Remote Lab Simulator', description: 'A simulator that allows students to conduct virtual lab experiments and gain hands-on experience remotely.', impact: 5, feasibility: 3, difficulty: 4},{title: 'Collaborative Study Space', description: 'An online space where students can study together, share resources, and support each other's learning.', impact: 3, feasibility: 5, difficulty: 2}]. Please generate the ideas. Never use new line. Never end with period`,
+					}
+				);
+				console.log(response.data);
+	
+				let generatedIdeas = JSON.parse(
+					response.data.response
+						.replace(/([{,]\s*)(\w+):/g, '$1"$2":')
+						.replace(/'/g, '"')
+				);
+	
+				generatedIdeas = generatedIdeas.map((object) => ({
+					...object,
+					category: option,
+				}));
+	
+				console.log(generatedIdeas);
+	
+				setIdeationProjectData(generatedIdeas);
+				setStep(step + 1);
+			} catch (error) {
+				console.error('Error generating ideas:', error);
+				setIsLoading(false)
+			} finally {
+				setIsLoading(false); // Reset loading state
+			}
 		};
 		generateIdeas();
 	};
-
+	
 	const regenerateProjectIdeas = () => {
+		setIsLoading(true); // Set loading state to true
 		const generateIdeas = async () => {
-			const response = await axios.post(
-				`${import.meta.env.VITE_GENESIS_API_URL}/api/chat`,
-				{
-					prompt: `Generate a list of 5 distinct project ideas given the category: ${option} and the issues: ${editedIssues}. Each idea should include a highly appropriate title with a creative name, a useful description, a highly accurate impact rating, a highly accurate feasibility rating, a highly accurate difficulty rating, and a list of 3 main features. The list should always be in the following format exactly: [{title: 'Virtual Classroom Enhancer', description: 'A tool to create interactive and engaging virtual classrooms with real-time collaboration features.', impact: 5, feasibility: 4, difficulty: 3, features: ['Real-time collaboration', 'Interactive quizzes', 'Virtual whiteboard']},{title: 'AI-Powered Tutoring Assistant', description: 'An AI-driven tutoring assistant that provides personalized help to students based on their learning progress.', impact: 4, feasibility: 3, difficulty: 4, features: ['Adaptive learning paths', 'Progress tracking', 'Automated feedback']},{title: 'Gamified Learning Platform', description: 'A platform that uses game mechanics to make remote learning more engaging and motivating for students.', impact: 4, feasibility: 3, difficulty: 3, features: ['Leaderboards', 'Achievement badges', 'Interactive challenges']},{title: 'Remote Lab Simulator', description: 'A simulator that allows students to conduct virtual lab experiments and gain hands-on experience remotely.', impact: 5, feasibility: 3, difficulty: 4, features: ['Virtual lab experiments', 'Real-time data analysis', 'Experiment tutorials']},{title: 'Collaborative Study Space', description: 'An online space where students can study together, share resources, and support each other's learning.', impact: 3, feasibility: 5, difficulty: 2, features: ['Shared resource library', 'Group study sessions', 'Peer support chat']}] Please generate the ideas. Never use new lines. Never end with period or comma`,
-				}
-			);
-			console.log(response.data);
-
-			let generatedIdeas = JSON.parse(
-				response.data.response
-					.replace(/([{,]\s*)(\w+):/g, '$1"$2":')
-					.replace(/'/g, '"')
-			);
-
-			generatedIdeas = generatedIdeas.map((object) => ({
-				...object,
-				category: option,
-			}));
-
-			console.log(generatedIdeas);
-
-			setIdeationProjectData(generatedIdeas);
+			try {
+				const response = await axios.post(
+					`${import.meta.env.VITE_GENESIS_API_URL}/api/chat`,
+					{
+						prompt: `Generate a list of 5 distinct project ideas given the category: ${option} and the issues: ${editedIssues}. Each idea should include a highly appropriate title with a creative name, a useful description, a highly accurate impact rating, a highly accurate feasibility rating, a highly accurate difficulty rating, and a list of 3 main features. The list should always be in the following format exactly: [{title: 'Virtual Classroom Enhancer', description: 'A tool to create interactive and engaging virtual classrooms with real-time collaboration features.', impact: 5, feasibility: 4, difficulty: 3, features: ['Real-time collaboration', 'Interactive quizzes', 'Virtual whiteboard']},{title: 'AI-Powered Tutoring Assistant', description: 'An AI-driven tutoring assistant that provides personalized help to students based on their learning progress.', impact: 4, feasibility: 3, difficulty: 4, features: ['Adaptive learning paths', 'Progress tracking', 'Automated feedback']},{title: 'Gamified Learning Platform', description: 'A platform that uses game mechanics to make remote learning more engaging and motivating for students.', impact: 4, feasibility: 3, difficulty: 3, features: ['Leaderboards', 'Achievement badges', 'Interactive challenges']},{title: 'Remote Lab Simulator', description: 'A simulator that allows students to conduct virtual lab experiments and gain hands-on experience remotely.', impact: 5, feasibility: 3, difficulty: 4, features: ['Virtual lab experiments', 'Real-time data analysis', 'Experiment tutorials']},{title: 'Collaborative Study Space', description: 'An online space where students can study together, share resources, and support each other's learning.', impact: 3, feasibility: 5, difficulty: 2, features: ['Shared resource library', 'Group study sessions', 'Peer support chat']}] Please generate the ideas. Never use new lines. Never end with period or comma`,
+					}
+				);
+				console.log(response.data);
+	
+				let generatedIdeas = JSON.parse(
+					response.data.response
+						.replace(/([{,]\s*)(\w+):/g, '$1"$2":')
+						.replace(/'/g, '"')
+				);
+	
+				generatedIdeas = generatedIdeas.map((object) => ({
+					...object,
+					category: option,
+				}));
+	
+				console.log(generatedIdeas);
+	
+				setIdeationProjectData(generatedIdeas);
+			} catch (error) {
+				console.error('Error regenerating ideas:', error);
+			} finally {
+				setIsLoading(false); // Reset loading state
+			}
 		};
 		generateIdeas();
 	};
-
+	
 	const backtrackStep = () => {
 		setStep(step - 1);
 	};
@@ -159,6 +175,20 @@ const IdeationProject = ({ closeModal }) => {
 	};
 
 	const renderModalContent = () => {
+		if (isLoading === true) {
+			return (
+				<div className="loading-screen">
+					<h2>Generating...</h2>
+					<div className="progress-container">
+						<div className="progress-bar-outer">
+							<div className="progress-bar-inner"></div>
+						</div>
+					</div>
+					{/* Add a spinner or animation here if desired */}
+				</div>
+			);
+		}
+	
 		switch (step) {
 			// When step is 0, go back to main modal
 			case 0:
